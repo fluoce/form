206f4f3c2cb0490b99db2196a1292f884fe9e3b1
fluoce <fluoce@gmail.com>
2026-04-27 06:02:36 +0530

    Add additional CORS origin for form service to support requests from 'https://form.fluoce.com', enhancing accessibility for external clients.

--- service/src/main.ts
+++ service/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://127.0.0.1:5500',
+      'https://form.fluoce.com',
     ],
     credentials: true,
   });
