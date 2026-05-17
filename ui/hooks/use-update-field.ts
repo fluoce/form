786621c6ffffee7a25fd481ef83e01a02d74ef33
fluoce <fluoce@gmail.com>
2026-05-17 23:06:31 +0530

    Add lodash and its types for improved utility functions; update useUpdateField hook to utilize lodash's isEqual for better field comparison before updates.

--- ui/hooks/use-update-field.ts
+++ ui/hooks/use-update-field.ts
@@ -3,9 +3,13 @@ import { useDebounceCallback } from "@/hooks/use-debounce"
 import { useFieldUpdate } from "@/hooks/use-field"
 import { updatePageField } from "@/provider/store/slice/page-fields-slice"
 import { FormFieldUpdateType } from "@/types/form-types"
+import isEqual from "lodash/isEqual"
+import { useAppSelector } from "@/provider/store"
 
 export function useUpdateField() {
   const dispatch = useDispatch()
+
+  const pageField = useAppSelector((state) => state.pageFields.fields)
 
   const { mutate } = useFieldUpdate()
 
@@ -17,6 +21,9 @@ export function useUpdateField() {
   )
 
   const updateField = (fieldId: string, data: FormFieldUpdateType) => {
+    const field = pageField?.find((f) => f?.id === fieldId)
+    if (!field) return
+    if (isEqual(field?.config, data?.config)) return
     dispatch(updatePageField({ fieldId, data }))
     debouncedUpdate(fieldId, data)
   }
