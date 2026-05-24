088552f58451f8d5c0884e762181b29acf86c43a
fluoce <fluoce@gmail.com>
2026-05-24 19:06:58 +0530

    Enhance FormResult component: add data refresh functionality with a button and tooltip, improve loading state handling, and update UI layout for better user experience.

--- ui/components/form/module/result.tsx
+++ ui/components/form/module/result.tsx
@@ -13,17 +13,26 @@ import { formatNumber } from "@/utils/formate-number"
 import { useParams } from "next/navigation"
 import {
   MapPinned,
+  RotateCw,
   TableCellsSplit,
   TabletSmartphone,
   ThumbsUp,
 } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { toast } from "sonner"
 
 const TABS: string[] = ["Overview", "Submissions"]
 
 export function FormResult() {
   const { formId } = useParams<{ formId: string }>()
 
-  const { data, isLoading } = useSubmits({ formId })
+  const { data, isLoading, refetch, isFetching } = useSubmits({ formId })
 
   if (isLoading) {
     return <PageSpinner />
@@ -31,14 +40,32 @@ export function FormResult() {
 
   return (
     <Tabs defaultValue={TABS[0]} className="gap-6 p-4">
-      <TabsList>
-        {TABS.map((t) => (
-          <TabsTab key={t} value={t}>
-            {t}
-          </TabsTab>
-        ))}
-      </TabsList>
-      <TabsPanels className="p-2">
+      <div className="flex items-center gap-2">
+        <TabsList>
+          {TABS.map((t) => (
+            <TabsTab key={t} value={t}>
+              {t}
+            </TabsTab>
+          ))}
+        </TabsList>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={() =>
+                refetch().then(() =>
+                  toast.success("Data Refreshed successfully !")
+                )
+              }
+              size="icon"
+              variant="outline"
+            >
+              <RotateCw className={cn(isFetching && "animate-spin")} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Refresh Data</TooltipContent>
+        </Tooltip>
+      </div>
+      <TabsPanels className={cn("p-2", isFetching && "opacity-50")}>
         <TabsPanel value={TABS[0]} className="flex flex-col gap-8">
           <div className="flex flex-col gap-4">
             <span className="flex items-center gap-2 text-sm">
