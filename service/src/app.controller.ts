c3e88065670c28678ead30f99255e8ae1e47e13a
Ashit mulani <mulaniashit@gmail.com>
2026-08-02 15:26:00 +0530

    Add public health endpoint to AppController

--- service/src/app.controller.ts
+++ service/src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Req } from '@nestjs/common';
+import { Public } from "@/decorator/public.decorator"
 
 @Controller('app')
 export class AppController {
   constructor() {}
 
+  @Public()
   @Get('health')
   async health() {
     return {
